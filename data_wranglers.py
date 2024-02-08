44279c0206fee425974094bdd31ee7842a4ee7af
Marc Green <marc1701@gmail.com>
2024-02-08 17:38:50 +0000

    Added mfcc dataset

--- data_wranglers.py
+++ data_wranglers.py
@@ -5,6 +5,7 @@ import torch
 import os
 import glob
 import pandas as pd
+import torchaudio, torchvision
 
 
 class UAVNoiseAffect(Dataset):
@@ -51,3 +52,47 @@ class UAVNoiseDataModule(pl.LightningDataModule):
     
     def val_dataloader(self):
         return DataLoader(self.val_data, 1, num_workers=19)
+    
+
+class DroneMFCCAffectPeak(torch.utils.data.Dataset):
+
+    def __init__(self,
+                 input_path='raw_data/Stimuli/',
+                 targets_file='mean_ratings.csv'):
+        
+        self.file_list = glob.glob(f'{input_path}*')
+        self.targets = pd.read_csv(targets_file, index_col=0)
+        self.resample = torchaudio.transforms.Resample(48_000, 16_000)
+        self.mfcc = torchaudio.transforms.MFCC(
+            n_mfcc=20, 
+            melkwargs={
+                "n_fft": 400, 
+                "hop_length": 4096, # 1/5th second
+                "n_mels": 20, 
+                "center": False
+            }
+        )
+        self.transform = torchvision.transforms.Compose([
+            self.resample, self.mfcc])
+
+    def __len__(self):
+        return len(self.targets)
+    
+    def __getitem__(self, item):
+        filepath = self.file_list[item]
+        x, _ = torchaudio.load(filepath)
+        x = x.sum(0)
+        x = self.transform(x)
+
+        # select peak frame
+        max_0 = torch.where(x[0] == torch.max(x[0]))
+        x = x[:, max_0]
+
+        index_str = (
+            os.path.basename(
+            filepath).split('.')[0]
+            .split('_C')
+        )[0]
+        y = self.targets.loc[index_str].values
+
+        return x, y
